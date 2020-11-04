7675c1379d51d6f9b4d82b5dc8958a20146f1f95
isaac <isaac@giuricich.ca>
2020-11-04 22:23:13 +0000

    jest done

--- price-calc.test.js
+++ price-calc.test.js
@@ -1,11 +1,51 @@
 const { TestScheduler } = require('jest')
 
 const priceCalc = require('./price-calc')
+const calculator = new priceCalc()
+
 const fs = require('fs')
 
-test('properly calculates the price of a pizza', () => {
-    let pizzaFile = fs.readFileSync('./resource/samplePizzas.json')
-    let lookup = fs.readFileSync('./resource/lookup.json')
-    console.log(pizzaFile)
-    console.log(lookup)
+let pizzaFile = fs.readFileSync('./resource/samplePizzas.json')
+let lookupFile = fs.readFileSync('./resource/lookup.json')
+let pizzas = JSON.parse(pizzaFile).pizzas
+let lookup = JSON.parse(lookupFile)
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[0].pizza, lookup)).toBe("13.44")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[1].pizza, lookup)).toBe("6.72")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[2].pizza, lookup)).toBe("14.56")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[3].pizza, lookup)).toBe("13.44")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[4].pizza, lookup)).toBe("11.20")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[5].pizza, lookup)).toBe("23.52")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[6].pizza, lookup)).toBe("15.12")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[7].pizza, lookup)).toBe("14.56")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[8].pizza, lookup)).toBe("28.00")
+})
+
+test('properly calculates the price of a pizza configuration', () => {
+    expect(calculator.calculate(pizzas[9].pizza, lookup)).toBe("21.28")
 })
